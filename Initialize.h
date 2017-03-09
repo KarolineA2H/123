15746fa57095634a54d69e43ed1ccead6a37a915
JohanneLun <johanne.kalland@outlook.com>
2017-03-09 12:42:38 +0100

    Lagt til kommentarer

--- Initialize.h
+++ Initialize.h
@@ -2,6 +2,11 @@
 #ifndef Initialize
 #define Initialize
 
+/*
+Initializes the elevator.
+Sets the initial value of last_moving_motor_dir and motor_dir to DIRN_DOWN.
+Also sets the floor indicator lamp.
+*/
 void elev_initialize();
 
 
