f235775ce62c1e3397ed5c754ebd5ae619343f4c
JohanneLun <johanne.kalland@outlook.com>
2017-03-09 12:53:36 +0100

    MotorManager.h kommentarer OK

--- MotorManager.h
+++ MotorManager.h
@@ -3,15 +3,24 @@
 
 #include <stdio.h>
 
-
-
+/*
+Sets static variable motor_dir to int passed to function.
+*/
 void mm_set_motor_dir (int motor_direction); 
+
+/*
+Returns static variable motor_dir
+*/
 int mm_get_motor_dir(); 
 
+/*
+Sets static variable last_moving_motor_dir to int passed to function
+*/
+void mm_set_last_moving_motor_dir(int motor_direction);
 
-void mm_set_last_moving_motor_dir(int motor_direction); 
+/*
+Returns static variable last_moving_motor_dir
+*/
 int mm_get_last_moving_motor_dir(); 
-
-
 
 #endif
