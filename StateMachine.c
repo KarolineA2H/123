1a21469aac979e2bc22e278fab38c69ddc8d0968
KarolineA2H <amalie.khh@gmail.com>
2017-03-12 21:48:39 +0100

    Fjernet unødvendige print. Kommentert noe kode som kan være utydelig. Har et problem med å kommentere der vi sammenlikner ordre og current floor. Men funnet også flere steder der koden er veldig lik og vi kunne gjort foranklinger

--- StateMachine.c
+++ StateMachine.c
@@ -4,8 +4,7 @@ static int current_floor;
 static int previous_floor;  
 static int stop_button_pressed;
 
-//kan ikke være mellom etg 
-//må kjøres relativt ofte for å kunne få med seg at etasjen skal/har skiftet
+
 void sm_set_current_floor_and_indicator(){
 	int floor = elev_get_floor_sensor_signal();
 
@@ -24,10 +23,9 @@ int sm_get_previous_floor(){
 	return previous_floor; 
 }
 
-//sjekker om en knapp er trykket og 
-//legger til i kø og setter på lys, hvis en er trykket
+
 void sm_check_button_pressed_up(){
-	qm_set_order_in_Q_up(TOP_FLOOR, 0); // denne knappen finnes ikke fysisk 
+	qm_set_order_in_Q_up(TOP_FLOOR, 0); // Not a physical button 
 
 	if (elev_get_button_signal(BUTTON_CALL_UP, GROUND_FLOOR) == 1) {
 		qm_set_order_in_Q_up(GROUND_FLOOR, 1);
@@ -44,7 +42,7 @@ void sm_check_button_pressed_up(){
 }
 
 void sm_check_button_pressed_down(){
-	qm_set_order_in_Q_down(GROUND_FLOOR, 0);
+	qm_set_order_in_Q_down(GROUND_FLOOR, 0);// Not a physical button
 
 	if (elev_get_button_signal(BUTTON_CALL_DOWN, FIRST) == 1) {
 		qm_set_order_in_Q_down(FIRST, 1);
@@ -79,23 +77,22 @@ void sm_check_button_pressed_command(){
 	}
 }
 
-//skjekker alle type bestillingsknapper og setter lyset 
+
 void sm_check_button_pressed(){
 	sm_check_button_pressed_up(); 
 	sm_check_button_pressed_down(); 
 	sm_check_button_pressed_command(); 
 } 
 
-//teller tre sek. men skjekker også knappene+
+
 void sm_timer_handler(){
 	t_start_timer(); 
-	//så lenge vi teller ned tre sekunder 
 	while(t_check_timer()!= 1){
 		sm_check_button_pressed(); 
 	}
 }
 
-//åpner døren i 3 sekunder og så lukker døren 
+ 
 void sm_door_handler(){
 	elev_set_door_open_lamp(1); 
 	sm_timer_handler(); 
@@ -104,43 +101,29 @@ void sm_door_handler(){
 
 void sm_order_in_Q_vs_current_floor(){
 	int floor = elev_get_floor_sensor_signal();
+	//If the elevator is at a floor 
 	if (floor != -1) {
+		//Check if it is an order an anny of the Q matches the current floor and motor direction 
 		if((qm_get_order_in_Q_up(current_floor) || qm_get_order_in_Q_command(current_floor)) && mm_get_motor_dir() == DIRN_UP) {
 			sm_when_stopping_at_floor();
 		}
 		if((qm_get_order_in_Q_down(current_floor) || qm_get_order_in_Q_command(current_floor)) && mm_get_motor_dir() == DIRN_DOWN ) {
 			sm_when_stopping_at_floor();
 		}
-		if(sm_check_Q_up_for_orders() == 0 && sm_check_Q_command_for_orders() == 0) { //for kjører nedover
+		
+		//If it is a order that wants to go down 
+		if(sm_check_Q_up_for_orders() == 0 && sm_check_Q_command_for_orders() == 0) { 
 			if (qm_get_order_in_Q_down(floor) == 1) {
 				sm_when_stopping_at_floor();
 			}
 		}
+		//If it is a order that wants to go up 
 		if(sm_check_Q_down_for_orders()== 0 && sm_check_Q_command_for_orders() == 0) { //for kjører nedover
 			if (qm_get_order_in_Q_up(floor) == 1) {
 				sm_when_stopping_at_floor();
 			}
 		}
 	}
-}
-
-//returnerer 1 hvis det er en order 
-int sm_check_Q_up_for_orders(){
-	for (int i = current_floor; i < 4; i++) {
-		if (qm_get_order_in_Q_up(i) == 1) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
-int sm_check_Q_down_for_orders(){
-	for (int i = current_floor; i > -1; i--) {
-		if (qm_get_order_in_Q_down(i) == 1) {
-			return 1;
-		}
-	}
-	return 0;
 }
 
 int sm_check_Q_command_for_orders(){
@@ -160,6 +143,28 @@ int sm_check_Q_command_for_orders(){
 	}
 	return 0;
 }
+
+int sm_check_Q_down_for_orders(){
+	for (int i = current_floor; i > -1; i--) {
+		if (qm_get_order_in_Q_down(i) == 1) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+int sm_check_Q_up_for_orders(){
+	for (int i = current_floor; i < 4; i++) {
+		if (qm_get_order_in_Q_up(i) == 1) {
+			return 1;
+		}
+	}
+	return 0;
+}
+
+
+
 
 void sm_when_stopping_at_floor() {
 	mm_set_motor_dir(DIRN_STOP); 
@@ -185,16 +190,12 @@ void sm_check_stop_button(){
 	}
 }
 
-//stopp knapp aktivert
-void sm_stop_button_activated_ignore_orders(){
-	//skruer på stopp lampen 
-	elev_set_stop_lamp(1);
-	//stopper motoren 
+
+void sm_stop_button_activated_ignore_orders(){ 
+	elev_set_stop_lamp(1); //Turn the stop-button on 
 	elev_set_motor_direction(DIRN_STOP);
-	//sletter køen 
-	//qm_delete_Q();
 	sm_reset_all_button_lamps_delete_Q();
-	//heisen står mellom to etg 	
+	
 	if (elev_get_floor_sensor_signal() == -1) {
 
 		while (elev_get_stop_signal() == 1) {
@@ -203,15 +204,13 @@ void sm_stop_button_activated_ignore_orders(){
 		elev_set_stop_lamp(0); 
 	}
 	else {
-		//stopp knappen ble aktivert når vi er en etg 
-		elev_set_door_open_lamp(1);
+		elev_set_door_open_lamp(1); 
 		while (elev_get_stop_signal() == 1) {
 			continue;
 		}
-		elev_set_stop_lamp(0); 
-		//døren er åpen i tre sekunder etter at vi har sluppet stop knappen men vi kan ta i mot ordere 
-		sm_timer_handler(); 
-		elev_set_door_open_lamp(0);
+		elev_set_stop_lamp(0); //Turn the stop-button off 
+		sm_timer_handler(); //Count to tre and check the ordering buttons 
+		elev_set_door_open_lamp(0); //Turn the door lamp of 
 	} 
 }
 
@@ -219,8 +218,7 @@ void sm_reset_all_button_lamps_delete_Q(){
 	qm_delete_Q(); 
 
 	int i;
-    // Zero all floor button lamps
-    for (i = 0; i < N_FLOORS; i++) { // burde henne ha hver implimentert i++ 
+    for (i = 0; i < N_FLOORS; i++) { 
         if (i != 0)
             elev_set_button_lamp(BUTTON_CALL_DOWN, i, 0);
 
@@ -233,8 +231,6 @@ void sm_reset_all_button_lamps_delete_Q(){
 
 int sm_elev_on_standby(){
 	if (qm_is_Q_empty() == 1){
-		//sette motoren til å stoppe
-
 		mm_set_motor_dir(DIRN_STOP); 
 		return 1;
 	}
@@ -244,39 +240,37 @@ int sm_elev_on_standby(){
 }
 
 void sm_elev_move_or_stop(){
-	if (sm_elev_on_standby()) {}
+	if (sm_elev_on_standby()) {} //The Q is empty, the moror dir =DRIN STOP
 
 	else {
-		if (elev_get_floor_sensor_signal() != -1) {
+		if (elev_get_floor_sensor_signal() != -1) {//If the elevator is on a floor 
 			sm_drive_direction();
 		}
-		else if(stop_button_pressed == 1){
+		else if(stop_button_pressed == 1){//It the elevator is between to floors, and the stop button is activated 
 
 			int DIR = mm_get_last_moving_motor_dir();
 
 			if(DIR == DIRN_UP ){
-			//Skjekker ordre over forrige etg
-
+				//Check if the first order is to a floor where the elevator just passed 
 				if (qm_get_order_in_Q_up(previous_floor) || qm_get_order_in_Q_down(previous_floor) || qm_get_order_in_Q_command(previous_floor)){
 					mm_set_motor_dir(DIRN_DOWN);
 					mm_set_last_moving_motor_dir(DIRN_DOWN);
-					printf("kom hit til dirn_up\n" );
 				}
 				else{
 					sm_drive_direction_between_floors();
 				}
 			}
 			else if(DIR == DIRN_DOWN){
-			//skjekker ordre under forrige etg 
+				//Check if the first order is to a floor where the elevator just passed
 				if (qm_get_order_in_Q_up(previous_floor) || qm_get_order_in_Q_down(previous_floor) || qm_get_order_in_Q_command(previous_floor)){
 					mm_set_motor_dir(DIRN_UP);
 					mm_set_last_moving_motor_dir(DIRN_UP);
-					printf("KOM HIT til DRIN DOWN \n");
 				}
 				else {	
 					sm_drive_direction_between_floors();
 				}
 			}
+			//The stop- button case is handeled 
 			stop_button_pressed = 0;
 		}
 		else {}
@@ -298,7 +292,7 @@ void sm_drive_direction(){
 			break; 
 
 		default:
-			printf("kom hit \n");
+			printf("Default last moving motor dir \n");
 		}
 }
 
@@ -306,17 +300,16 @@ void case_dirn_up() {
 	int floor = elev_get_floor_sensor_signal(); 
 
 	if (floor != -1){
-		int smth_in_Q_over = 0;
+		int smth_in_Q_over = 0;//variabel; something in the  
 
-		for (int j = floor; j < 4; j++){
+		for (int j = floor; j < 4; j++){ //Check if it is some orders over the elevator 
 			if (qm_get_order_in_Q_up(j) || qm_get_order_in_Q_down(j) || qm_get_order_in_Q_command(j)) {
 				mm_set_motor_dir(DIRN_UP);
-				//mm_set_last_moving_motor_dir(DIRN_UP);
 				smth_in_Q_over = 1;
 				break;
 			}
 		}
-		if (smth_in_Q_over == 0){
+		if (smth_in_Q_over == 0){//Know there is a order under the elev   
 			mm_set_motor_dir(DIRN_DOWN);
 			mm_set_last_moving_motor_dir(DIRN_DOWN);
 		}
@@ -328,19 +321,17 @@ void case_dirn_down(){
 	int floor = elev_get_floor_sensor_signal(); 
 
 	if (floor != -1){
-		int smth_in_Q_under = 0;
+		int smth_in_Q_under = 0;//variabel; something in the 
 
-		for (int i = floor ; i > -1; i--){
+		for (int i = floor ; i > -1; i--){//Check if it is some orders over the elevator 
 			if (qm_get_order_in_Q_up(i) || qm_get_order_in_Q_down(i) || qm_get_order_in_Q_command(i)) {
 				mm_set_motor_dir(DIRN_DOWN);
-				//mm_set_last_moving_motor_dir(DIRN_DOWN);
 				smth_in_Q_under = 1;
 				break;
 			}
 		}
 		
-		if (smth_in_Q_under == 0) {
-			//vet vi skal oppover 
+		if (smth_in_Q_under == 0) { //Know there is a order over the elev  
 			mm_set_motor_dir(DIRN_UP);
 			mm_set_last_moving_motor_dir(DIRN_UP);
 		}
@@ -365,47 +356,44 @@ void sm_drive_direction_between_floors(){
 			break; 
 
 		default:
-			printf("kom hit \n");
+			printf("Default; last moving dir \n");
 		}
 }
 
 void case_dirn_up_between_floors() {
-	int smth_in_Q_over = 0;	
+	int smth_in_Q_over = 0;	//variabel; something in the Q 
 		
-	for (int j = previous_floor+1; j < 4; j++){
+	for (int j = previous_floor+1; j < 4; j++){//Check if it is some orders over the elevator
 		if (qm_get_order_in_Q_up(j) || qm_get_order_in_Q_down(j) || qm_get_order_in_Q_command(j)) {
 			mm_set_motor_dir(DIRN_UP);
-			//mm_set_last_moving_motor_dir(DIRN_UP);
 			smth_in_Q_over = 1;
-			printf("ordre er over. kjør opp\n");
+			
 			break;
 		}
 	}
-	if (smth_in_Q_over == 0){
+	if (smth_in_Q_over == 0){ //Know there is a order over the elev 
 		mm_set_motor_dir(DIRN_DOWN);
 		mm_set_last_moving_motor_dir(DIRN_DOWN);
-		printf("kjørte opp. men nå kjører vi ned :O"); 
+		 
 	}
 	else {}
 }
 
 void case_dirn_down_between_floors(){
-	int smth_in_Q_under = 0;
+	int smth_in_Q_under = 0; //variabel; something in the 
 
-	for (int i = previous_floor-1; i > -1; i--){
+	for (int i = previous_floor-1; i > -1; i--){//Check if it is some orders under the elevator
 		if (qm_get_order_in_Q_up(i) || qm_get_order_in_Q_down(i) || qm_get_order_in_Q_command(i)) {
 			mm_set_motor_dir(DIRN_DOWN);
-			//mm_set_last_moving_motor_dir(DIRN_DOWN);
 			smth_in_Q_under = 1;
-			printf("kjører nedover, er en ordre under \n");
+
 			break;
 		}
 	}
-	if (smth_in_Q_under == 0) {
-		//vet vi skal oppover 
+	if (smth_in_Q_under == 0) {//Know there is a order over the elev 
 		mm_set_motor_dir(DIRN_UP);
 		mm_set_last_moving_motor_dir(DIRN_UP);
-		printf("kjørte ned. men nå kjører vi opp :D \n");
+		
 	}
 	else{}
 }
